d2ad097f82a3ff61cdf4662264b2ddc2c79d4e7c
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-01-18 12:32:51 +0100

    added metric action

--- Apps.ContentQuo/Actions/EvaluationsActions.cs
+++ Apps.ContentQuo/Actions/EvaluationsActions.cs
@@ -70,6 +70,25 @@ public class EvaluationsActions : BaseInvocable
         };
     }
 
+    [Action("Get evaluation metric", Description = "Get the grade and quality score of an evaluation. In case of multiple metrics, the first configured metric is shown.")]
+    public async Task<EvaluationMetricResponse> GetEvaluationMetrics([ActionParameter] GetEvaluationRequest input)
+    {
+        var request = new RestRequest($"/evaluations/{input.Id}/metrics", Method.Get);
+        var response = await _client.ExecuteAsync<MetricsDto>(request);
+
+        if (response.Data.Metrics == null || response.Data.Metrics.Count() < 1)
+            throw new Exception("ContentQuo Evaluate did not respond with any metrics. Please contact ContentQuo to get your instance configured.");
+
+        var usedMetric = response.Data.Metrics.First();
+
+        return new EvaluationMetricResponse()
+        {
+            Grade = usedMetric.Grade,
+            Name = usedMetric.Name,
+            QualityScore = usedMetric.QualityScore,
+        };
+    }
+
     [Action("Download evaluation report", Description = "Download the generated evaluation report. Defaults to an .xlsx file but can be cusotmized by ContentQuo.")]
     public async Task<DownloadFileResponse> DownloadEvaluationReport([ActionParameter] GetEvaluationRequest input, [ActionParameter] OptionalReportId reportId)
     {
---
+++ Apps.ContentQuo/Models/Responses/EvaluationMetricResponse.cs
@@ -0,0 +1,21 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.ContentQuo.Models.Responses
+{
+    public class EvaluationMetricResponse
+    {
+        [Display("Grade")]
+        public string Grade { get; set; }
+
+        [Display("Name")]
+        public string Name { get; set; }
+
+        [Display("Quality score")]
+        public int QualityScore { get; set; }
+    }
+}
